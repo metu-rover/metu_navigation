a6a178235a7552101874ca8a74973992798abe64
adnanhd <adnanharundogan@gmail.com>
2020-09-01 01:59:09 +0300

    migrated Detection -> leo_rover_detection

--- Detection/priz.py
+++
@@ -1,9 +0,0 @@
-from detecto import core, utils, visualize
-from detecto.visualize import detect_video
-
-import cv2
-import torch
-
-model = core.Model.load('model_weights.pth', ['mavi_priz', 'main_switch', 'voltage_knob'])
-
-visualize.detect_live(model)
